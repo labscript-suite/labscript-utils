b05d7c99af3a4dbe645d789ad0efb12c1ab29b5b
Phil Starkey <philipstarkey@users.noreply.github.com>
2012-02-16 05:05:21 +0000

    Modified Calibrations name to Unit conversions Renamed "connected to" to "parent port" in connection table and related code
    Added a UnitConversionBase class which handles order of magnitude conversions (eg to p,n,u,m,k,M,G,T SI-prefixes) automatically. Just make your conversion class a subclass of UniConversionBase and pass you conversion a 'magnitudes' dictionary key with the value containing a list of prefixes as strings (eg {'magnitudes':['k','G'],...})

---
+++ UnitConversionBase.py
@@ -0,0 +1,71 @@
+# -*- coding: UTF-8 -*-
+import copy
+import new
+import math
+
+class UnitConversion(object):
+    unit_list = {'p':'10**-12','n':'10**-9','u':'10**-6','m':'10**-3',
+                 'k':'10**3','M':'10**6','G':'10**9','T':'10**12'}
+    def __init__(self,params):
+        magnitudes = []
+        if 'magnitudes' in params:
+            magnitudes = params['magnitudes']
+        
+        # order the unit order of magnitudes
+        # We want lowest (negative) to -3 and then largest positive to 3
+        temp_units = []
+        for unit in magnitudes:
+            try:
+                if unit == 'µ':
+                    unit = 'u'
+                magnitude = math.log10(eval(self.unit_list[unit]))
+                if magnitude < 0:
+                    position = 0
+                    for u in temp_units:
+                        if math.log10(eval(u[1])) > magnitude:
+                            break
+                        else:
+                            position += 1
+                    
+                else:
+                    position = 0
+                    for u in temp_units:                        
+                        if math.log10(eval(u[1])) < 0:
+                            position += 1
+                        elif math.log10(eval(u[1])) > magnitude:
+                            position += 1
+                        else:
+                            break
+                
+                temp_units.insert(position,(unit,self.unit_list[unit]))
+            except Exception, e:
+                print e
+                pass
+                
+        self.units = temp_units
+        
+        for unit in self.units:
+            for derived_unit in self.derived_units:
+                #if derived_unit == unit[2]:
+                exec("def "+unit[0]+derived_unit+"_to_base(self,value): return self."+derived_unit+"_to_base(value)*"+unit[1])
+                exec ("a="+unit[0]+derived_unit+"_to_base")
+                self.__dict__[unit[0]+derived_unit+"_to_base"] = new.instancemethod(a,self,UnitConversion)
+                exec("def "+unit[0]+derived_unit+"_from_base(self,value): return self."+derived_unit+"_from_base(value/float("+unit[1]+"))")
+                exec ("a="+unit[0]+derived_unit+"_from_base")
+                self.__dict__[unit[0]+derived_unit+"_from_base"] = new.instancemethod(a,self,UnitConversion)
+        
+        # Make another loop to stop infinite regression!
+        derived_copy = copy.copy(self.derived_units)
+        for unit in self.units:
+            for derived_unit in derived_copy:        
+                # Add unit to derived unit list (put in correct order)
+                # Find derived unit location in list
+                pos = self.derived_units.index(derived_unit)
+                if math.log10(eval(unit[1])) > 0:
+                    pos+=1
+                # Is this unit a negative or positive order of magnitude?
+                
+                #if unit[0] == 'u':
+                #    self.derived_units.insert(pos,'µ'+derived_unit)
+                #else:
+                self.derived_units.insert(pos,unit[0]+derived_unit)
