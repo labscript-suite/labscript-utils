07673fe590bfa8af389b375e0441da017db48977
Chris Billington <chrisjbillington@gmail.com>
2012-03-09 04:34:31 +0000

    Added screenshot and shared_drive modues. Screenshot takes screenshots, shared_drive works out what the prefix for the shared drive is on both windows and linux

---
+++ __init__.py
@@ -0,0 +1,33 @@
+import os,sys,subprocess,time,itertools
+
+def get_prefix_linux(share):
+    home = os.environ['HOME']
+    gvfs = os.path.join(home,'.gvfs')
+    for mount in os.listdir(gvfs):
+        thisshare, on, thisserver = mount.split()
+        if thisshare == share:
+            return os.path.join(gvfs,mount)
+    raise RuntimeError('Share isn\'t mounted')
+    
+def get_prefix_win(server):
+    import win32com.client
+    def grouper(n, iterable, fillvalue=None):
+        "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+        args = [iter(iterable)] * n
+        return itertools.izip_longest(fillvalue=fillvalue, *args)
+    network = win32com.client.Dispatch('WScript.Network')            
+    drives = network.EnumNetworkDrives()
+    result = dict(grouper(2, drives))
+    for drive_letter,network_path in result.items():
+        if server in network_path:
+            return drive_letter
+    raise RuntimeError('Share isn\'t mounted')
+    
+def get_prefix(share):
+    if os.name == 'nt':
+        pass
+    elif 'linux' in sys.platform:
+        return get_prefix_linux(share)
+
+if __name__ == '__main__':
+    print 'monashbec prefix is:', get_prefix('monashbec')
