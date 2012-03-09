92303745088600531311615aafb9a08db1b5a9fd
Chris Billington <chrisjbillington@gmail.com>
2012-03-09 04:49:29 +0000

    Made screenshotting and getting of the shareddrive prefix work in windows

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def get_prefix_linux(share):
             return os.path.join(gvfs,mount)
     raise RuntimeError('Share isn\'t mounted')
     
-def get_prefix_win(server):
+def get_prefix_win(share):
     import win32com.client
     def grouper(n, iterable, fillvalue=None):
         "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
@@ -19,15 +19,16 @@ def get_prefix_win(server):
     drives = network.EnumNetworkDrives()
     result = dict(grouper(2, drives))
     for drive_letter,network_path in result.items():
-        if server in network_path:
+        if share in network_path:
             return drive_letter
     raise RuntimeError('Share isn\'t mounted')
     
 def get_prefix(share):
     if os.name == 'nt':
-        pass
+        return get_prefix_win(share)
     elif 'linux' in sys.platform:
         return get_prefix_linux(share)
-
+    else:
+        raise OSError('Can\'t get shareed drive prefix on this platform')
 if __name__ == '__main__':
     print 'monashbec prefix is:', get_prefix('monashbec')
