d1c25fe8239ed1bbe8673837c7691ef1178312ee
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2012-04-24 14:21:30 +0000

    Fixed bug in detuning.py - couldn't set default conversion parameter as nan (runmanager complained that nan wasn't a global - bug?). Now use False as default instead, used in conditionals that decide whether or not unit conversion has been defined in a certain way.

--- detuning.py
+++ detuning.py
@@ -9,19 +9,19 @@ class detuning(UnitConversion):
         self.parameters.setdefault('pass',1)        # specify single- or double-pass with sign
         self.parameters.setdefault('detuning_0',0)  # detuning of input light
         self.parameters.setdefault('gamma',6.065)   # natural linewidth in MHz
-        self.parameters.setdefault('aom_f0',NaN)    # rf frequency corrresponding to resonance
+        self.parameters.setdefault('aom_f0',False)    # rf frequency corrresponding to resonance
         
         UnitConversion.__init__(self,self.parameters)
 
     def d_MHz_to_base(self,detuning):
-        if isnan(self.parameters['aom_f0']):
+        if not self.parameters['aom_f0']:
             aom_frequency = (detuning - self.parameters['detuning_0']) / self.parameters['pass']
         else:
             aom_frequency = detuning / self.parameters['pass'] + self.parameters['aom_f0']
         return aom_frequency
         
     def d_MHz_from_base(self,aom_frequency):
-        if isnan(self.parameters['aom_f0']):
+        if not self.parameters['aom_f0']:
             detuning = self.parameters['pass'] * aom_frequency + self.parameters['detuning_0']
         else:
             detuning = self.parameters['pass'] * (aom_frequency - self.parameters['aom_f0']) 
