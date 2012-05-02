61d9254a8a115bf8f16779e1b15640ec19747f4e
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2012-05-02 02:58:03 +0000

    Modified unit conversion detuning.py to account for change of base units from MHz to Hz. Now have a conversion to MHz also.

--- detuning.py
+++ detuning.py
@@ -1,7 +1,7 @@
 from UnitConversionBase import *
 class detuning(UnitConversion):
-    base_unit = 'MHz'
-    derived_units = ['d_MHz', 'linewidths']
+    base_unit = 'Hz'
+    derived_units = ['MHz', 'd_MHz', 'linewidths']
     
     def __init__(self,calibration_parameters = None):            
         self.parameters = calibration_parameters
@@ -9,28 +9,36 @@ class detuning(UnitConversion):
         self.parameters.setdefault('pass',1)        # specify single- or double-pass with sign
         self.parameters.setdefault('detuning_0',0)  # detuning of input light
         self.parameters.setdefault('gamma',6.065)   # natural linewidth in MHz
-        self.parameters.setdefault('aom_f0',False)    # rf frequency corrresponding to resonance
+        self.parameters.setdefault('aom_f0',False)  # rf frequency corrresponding to resonance
         
         UnitConversion.__init__(self,self.parameters)
 
+    def MHz_to_base(self,aom_frequency_MHz):
+        return 1e6*aom_frequency_MHz
+        
+    def MHz_from_base(self,aom_frequency):
+        return 1e-6*aom_frequency
+        
     def d_MHz_to_base(self,detuning):
         if not self.parameters['aom_f0']:
-            aom_frequency = (detuning - self.parameters['detuning_0']) / self.parameters['pass']
+            aom_frequency_MHz = (detuning - self.parameters['detuning_0']) / self.parameters['pass']
         else:
-            aom_frequency = detuning / self.parameters['pass'] + self.parameters['aom_f0']
-        return aom_frequency
+            aom_frequency_MHz = detuning / self.parameters['pass'] + self.parameters['aom_f0']
+        return self.MHz_to_base(aom_frequency_MHz)
         
     def d_MHz_from_base(self,aom_frequency):
+        aom_frequency_MHz = self.MHz_from_base(aom_frequency)
         if not self.parameters['aom_f0']:
-            detuning = self.parameters['pass'] * aom_frequency + self.parameters['detuning_0']
+            detuning = self.parameters['pass'] * aom_frequency_MHz + self.parameters['detuning_0']
         else:
-            detuning = self.parameters['pass'] * (aom_frequency - self.parameters['aom_f0']) 
+            detuning = self.parameters['pass'] * (aom_frequency_MHz - self.parameters['aom_f0']) 
         return detuning
         
     def linewidths_to_base(self,linewidths):
-        aom_frequency = self.d_MHz_to_base(self.parameters['gamma'] * linewidths)
-        return aom_frequency
+        aom_frequency_MHz = self.d_MHz_to_base(self.parameters['gamma'] * linewidths)
+        return self.MHz_to_base(aom_frequency_MHz)
         
     def linewidths_from_base(self,aom_frequency):
-        linewidths = self.d_MHz_from_base(aom_frequency) / self.parameters['gamma']
+        aom_frequency_MHz = self.MHz_from_base(aom_frequency)
+        linewidths = self.d_MHz_from_base(aom_frequency_MHz) / self.parameters['gamma']
         return linewidths
