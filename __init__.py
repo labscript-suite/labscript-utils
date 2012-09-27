f3caa31a67e7f5efb2ca3414b69fde89242156a8
Chris Billington <chrisjbillington@gmail.com>
2012-09-27 12:31:19 +0000

    Removed all shared drive mounting and finding functionality. Now all this module does is convert paths from local paths to platform agnostic ones and vice versa.
    So if programs send paths over the network, they first call shared_drive.path_to_agnostic on it, and when programs receive paths, they call path_to_local before attempting to access the file.
    
    This process is performed manually in many of our programs, they will slowly be migrated to use the same module, as is neater.

--- __init__.py
+++ __init__.py
@@ -1,53 +1,18 @@
-import os,sys,subprocess,time,itertools
+import os
+from LabConfig import LabConfig
 
-def get_prefix_linux(share):
-    home = os.environ['HOME']
-    gvfs = os.path.join(home, '.gvfs')
-    # check if gvfs has been used to mount the filesystem
-    if os.path.exists(gvfs):
-        for mount in os.listdir(gvfs):
-            if share in mount:
-                return os.path.join(gvfs, mount)
-    # otherwise check conventional locations
-    with open('/proc/mounts') as f:
-        mounts = f.read().split('\n')
-    for mount in mounts:
-        if len(mount.split()) > 1:
-            file_system, mount_point = mount.split()[:2]
-            if share in file_system:
-                return mount_point
-    raise RuntimeError('Share isn\'t mounted')
-    
-def get_prefix_win(share):
-    import win32com.client
-    def grouper(n, iterable, fillvalue=None):
-        "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-        args = [iter(iterable)] * n
-        return itertools.izip_longest(fillvalue=fillvalue, *args)
-    network = win32com.client.Dispatch('WScript.Network')            
-    drives = network.EnumNetworkDrives()
-    result = dict(grouper(2, drives))
-    for drive_letter,network_path in result.items():        
-        if drive_letter and share in network_path:
-            return drive_letter + os.sep
-    raise RuntimeError('Share isn\'t mounted')
+_config = LabConfig(required_params={'paths':['shared_drive']})
+_prefix = _config.get('paths','shared_drive')
 
-def get_prefix_macos(share):
-    volumes = os.path.join('/Volumes')
-    for mount in os.listdir(volumes):
-        if share in mount:
-            return os.path.join(volumes, mount)
-    raise RuntimeError('Share isn\'t mounted')
-        
-def get_prefix(share):
-    if os.name == 'nt':
-        return get_prefix_win(share)
-    elif 'linux' in sys.platform:
-        return get_prefix_linux(share)
-    elif 'darwin' in sys.platform:
-        return get_prefix_macos(share)
-    else:
-        raise OSError('Can\'t get shareed drive prefix on this platform')
+def path_to_agnostic(path):
+    path = os.path.abspath(path)
+    if path.startswith(_prefix):
+        path = path.replace(_prefix,'Z:')
+    path = path.replace(os.path.sep,'\\')
+    return path
     
-if __name__ == '__main__':
-    print 'monashbec prefix is:', get_prefix('monashbec')
+def path_to_local(path):
+    path = path.replace('\\', os.path.sep)
+    if path.startswith('Z:'):
+        path = path.replace('Z:',_prefix)
+    return path
