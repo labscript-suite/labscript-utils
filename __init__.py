c39b3cdc6799097d8b48e44d2edfde5a137f1dba
Chris Billington <chrisjbillington@gmail.com>
2012-09-27 12:28:43 +0000

    Completely overhauled to use zlock instead of redis. Seems to work well!

--- __init__.py
+++ __init__.py
@@ -1,37 +1,62 @@
+import os
 import sys
-from redis import Redis
+import socket
+import subprocess
 
-def turn_locks_on(redis_server='localhost'):
+import zmq
 
-    if 'h5py' in sys.modules:
-        raise ImportError('h5_lock.turn_locks_on must be celled prior to importing h5py')
+import zlock
+import shared_drive
+from LabConfig import LabConfig
+
+if 'h5py' in sys.modules:
+    raise ImportError('h5_lock must be imported prior to importing h5py')
         
-    import h5py
+import h5py
 
-    _orig_init = h5py.File.__init__
-    _orig_close = h5py.File.close
+def __init__(self, name, mode=None, driver=None, libver=None, **kwds):
+    key = shared_drive.path_to_agnostic(name)
+    self.zlock = zlock.Lock(key)
+    self.zlock.acquire()
+    _orig_init(self, name, mode, driver, libver, **kwds)
+
+def close(self):
+    self.zlock.release()
+    _orig_close(self)
     
-    def __init__(self, name, mode=None, driver=None, libver=None, **kwds):
-        self.redislock = None
-        if mode != 'r':
-            self.redislock = redis.lock(name)
-            self.redislock.acquire()
-        _orig_init(self, name, mode, driver, libver, **kwds)
-
-    def close(self):
-        if self.redislock is not None:
-            self.redislock.release()
-        _orig_close(self)  
+_orig_init = h5py.File.__init__
+_orig_close = h5py.File.close
+    
+# setup connection with the zlock server, depending on labconfig settings: 
+config = LabConfig(required_params={'ports':['zlock'],'servers':['zlock']})
+host = config.get('servers','zlock')
+port = config.get('ports','zlock')
+if socket.gethostbyname(host) == socket.gethostbyname('localhost'):
+    try:
+        # short connection timeout if localhost, don't want to
+        # waste time:
+        zlock.connect(host,port,timeout=0.1)
+    except zmq.ZMQError:
+        # No zlock server running on localhost. Start one:
+        devnull = open(os.devnull,'w')
+        global _server_process
+        _server_process = subprocess.Popen([sys.executable,'-m','zlock',port], stdout=devnull, stderr=devnull)
+        # Try again. Longer timeout this time, give it time to start up:
+        zlock.connect(host,port,timeout=15)
+else:
+    zlock.connect(host, port)
         
-    redis = Redis(redis_server)
-    h5py.File.__init__ = __init__
-    h5py.File.close = close        
+# Replace the h5py File open and close methods with our own, brand
+# new shiny locking ones:
+h5py.File.__init__ = __init__
+h5py.File.close = close 
+            
+def set_default_timeout(t):
+    zlock.set_default_timeout(t)
 
-if __name__ == '__main__':
-    import time
-    turn_locks_on()
-    import h5py
-    with h5py.File('a') as f:
-        print 'starting!'
-        time.sleep(5)
-        print 'ending!'
+# 30 seconds seems like a good timeout. The user can increase by calling
+# set_default_timeout themselves:
+
+set_default_timeout(30)
+
+
