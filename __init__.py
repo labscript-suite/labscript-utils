e578f0e6bf81115fb88f91888b35a745a12008df
Chris Billington <chrisjbillington@gmail.com>
2012-09-28 05:00:07 +0000

    Made prefix attribute public

--- __init__.py
+++ __init__.py
@@ -2,17 +2,17 @@ import os
 from LabConfig import LabConfig
 
 _config = LabConfig(required_params={'paths':['shared_drive']})
-_prefix = _config.get('paths','shared_drive')
+prefix = _config.get('paths','shared_drive')
 
 def path_to_agnostic(path):
     path = os.path.abspath(path)
-    if path.startswith(_prefix):
-        path = path.replace(_prefix,'Z:')
+    if path.startswith(prefix):
+        path = path.replace(prefix,'Z:')
     path = path.replace(os.path.sep,'\\')
     return path
     
 def path_to_local(path):
     path = path.replace('\\', os.path.sep)
     if path.startswith('Z:'):
-        path = path.replace('Z:',_prefix)
+        path = path.replace('Z:',prefix)
     return path
