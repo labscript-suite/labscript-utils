540034503169922c5c7aa3df68f58bdb0aa4ec83
Chris Billington <chrisjbillington@gmail.com>
2012-10-10 00:35:54 +0000

    Fixed a typo

--- horrible_fixed_length_strings_hack.py
+++ horrible_fixed_length_strings_hack.py
@@ -5,7 +5,7 @@ def horribly_hack_fixed_length_strings():
 
     def guess_dtype(data):
         if type(data) not in [bytes, unicode]:
-            return _guess_type(data)
+            return _guess_dtype(data)
             
     # I feel dirty:
     h5py._hl.base.guess_dtype = guess_dtype
