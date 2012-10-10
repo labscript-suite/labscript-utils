d068e05d06b358f62f8140a1d59004dc22ce5c00
Chris Billington <chrisjbillington@gmail.com>
2012-10-10 06:36:15 +0000

    moved imshow_irreg to analysislib.common, moved memprof into tracelog. This is so that there are no single files at the toplevel of pythonlib, in preparation of converting it to a mercurial repository full of subprepositories. Also removed bibfix...will add it again in a second, its being a little problematic not letting me commit while it exists. I just updated the version of subversion Im using, might have something to do with it

---
+++ memprof.py
@@ -0,0 +1,40 @@
+import gc
+
+def count_types():
+    types = {}
+    for object in gc.get_objects():
+        try:
+            c = object.__class__
+        except AttributeError:
+            c = type(object)
+        try:
+            types[c] += 1
+        except KeyError:
+            types[c] = 1
+    write_to_file(types)
+    return types
+    
+def write_to_file(types):
+    with open('memprof.txt','w') as f:
+        names = types.keys()
+        names.sort(key=lambda name: -types[name])
+        for name in names:
+            f.write(str(name).rjust(60) + ' ' + str(types[name]).rjust(8) + '\n')
+            
+def start():
+    global initial_counts
+    initial_counts = count_types()
+          
+def check():
+    diffs = {}
+    types = count_types()
+    for type in types:
+        try:
+            diffs[type] = types[type] - initial_counts[type]
+        except KeyError:
+            diffs[type] = types[type]
+    write_to_file(diffs)
+    return True
+    
+start()
+
