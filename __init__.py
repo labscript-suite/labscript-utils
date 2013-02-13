117c09476398c8b229b40ee1c42addc61ad7851d
Chris Billington <chrisjbillington@gmail.com>
2013-02-13 14:54:59 +1100

    Fixed the wrong number of slashes in path conversion.

--- __init__.py
+++ __init__.py
@@ -4,15 +4,21 @@ from LabConfig import LabConfig
 _config = LabConfig(required_params={'paths':['shared_drive']})
 prefix = _config.get('paths','shared_drive')
 
+Z = 'Z:\\\\'
 def path_to_agnostic(path):
     path = os.path.abspath(path)
     if path.startswith(prefix):
-        path = path.replace(prefix,'Z:')
-    path = path.replace(os.path.sep,'\\')
+        path = os.path.sep.join(path.split(prefix)[1:])
+        while path.startswith(os.path.sep):
+            path = path[1:]
+        path = path.replace(os.path.sep,'\\')
+        path = Z + path
     return path
     
 def path_to_local(path):
-    path = path.replace('\\', os.path.sep)
-    if path.startswith('Z:'):
-        path = path.replace('Z:',prefix)
+    if path.startswith(Z):
+        path = '\\'.join(path.split(Z)[1:])
+        path = path.replace('\\', os.path.sep)
+        path = os.path.join(prefix, path)
     return path
+    
