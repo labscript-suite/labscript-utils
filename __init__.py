a7ee2cb5ee88919705e297b024e2b7d8f6428be4
Chris Billington <chrisjbillington@gmail.com>
2013-02-18 00:14:14 +1100

    Removed debug code that was left in. Ensured it still works if the shared drive prefix doesn't have a slash at the end

--- __init__.py
+++ __init__.py
@@ -4,21 +4,28 @@ from LabConfig import LabConfig
 _config = LabConfig(required_params={'paths':['shared_drive']})
 prefix = _config.get('paths','shared_drive')
 
+# ensure prefix ends with a slash:
+if not prefix.endswith(os.path.sep):
+    prefix += os.path.sep
+    
 def path_to_agnostic(path):
     path = os.path.abspath(path)
     if path.startswith(prefix):
-        path = os.path.sep.join(path.split(prefix)[1:])
+        path = path.split(prefix, 1)[1]
         path = os.path.join('Z:', path)
         path = path.replace(os.path.sep,'\\')
     return path
     
 def path_to_local(path):
     if path.startswith('Z:'):
-        path = '\\'.join(path.split('Z:\\')[1:])
+        path = path.split('Z:\\', 1)[1]
         path = path.replace('\\', os.path.sep)
         path = os.path.join(prefix, path)
     return path
-    
-path = os.path.join(prefix, 'foo/bar/baz/qux')
-x = path_to_agnostic(path)
-y = path_to_local(x)
+
+if __name__ == '__main__':
+    # test:    
+    path = os.path.join(prefix, 'foo/bar/baz/qux')
+    agnostic_path = path_to_agnostic(path)
+    local_path = path_to_local(agnostic_path)
+    assert local_path == path
