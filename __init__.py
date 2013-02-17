b18847720d574826c409016f5fc41d7dda379d1f
Chris Billington <chrisjbillington@gmail.com>
2013-02-17 18:14:11 +1100

    Still had the wrong number of slashes. Another attempt.

--- __init__.py
+++ __init__.py
@@ -4,21 +4,21 @@ from LabConfig import LabConfig
 _config = LabConfig(required_params={'paths':['shared_drive']})
 prefix = _config.get('paths','shared_drive')
 
-Z = 'Z:\\\\'
 def path_to_agnostic(path):
     path = os.path.abspath(path)
     if path.startswith(prefix):
         path = os.path.sep.join(path.split(prefix)[1:])
-        while path.startswith(os.path.sep):
-            path = path[1:]
+        path = os.path.join('Z:', path)
         path = path.replace(os.path.sep,'\\')
-        path = Z + path
     return path
     
 def path_to_local(path):
-    if path.startswith(Z):
-        path = '\\'.join(path.split(Z)[1:])
+    if path.startswith('Z:'):
+        path = '\\'.join(path.split('Z:\\')[1:])
         path = path.replace('\\', os.path.sep)
         path = os.path.join(prefix, path)
     return path
     
+path = os.path.join(prefix, 'foo/bar/baz/qux')
+x = path_to_agnostic(path)
+y = path_to_local(x)
