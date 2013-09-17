dbfb3a47983427c405d03258039206685e87548e
Chris Billington <chrisjbillington@gmail.com>
2013-09-17 16:50:31 +1000

    Applied a fix for a bug whereby modulewatcher might unload modules whilst an import is in progress, leading to potential inconsistencies. Now it holds the import lock whilst it modifies sys.modules.
    Also added an attribute unloader_lock, which users should hold whenever they want to disable the automatic unloading of modules.

--- modulewatcher.py
+++ modulewatcher.py
@@ -2,6 +2,7 @@ import sys
 import threading
 import time
 import os
+import imp
 import gc
 
 class ModuleWatcher(object):
@@ -10,13 +11,17 @@ class ModuleWatcher(object):
         # if our check_and_unload function runs at the same time as a
         # garbage collection cycle. We will manually trigger garbage collection.
         gc.disable()
-                
+           
+        # A lock to hold whenever you don't want modules unloaded:
+        self.unloader_lock = threading.Lock()
+            
         # The whitelist is the list of names of currently loaded modules:
         self.whitelist = set(sys.modules)
         self.modified_times = {}
         self.main = threading.Thread(target=self.mainloop)
         self.main.daemon = True
         self.main.start()
+        
          
     def mainloop(self):
         while True:
@@ -40,21 +45,32 @@ class ModuleWatcher(object):
                 if modified_time != previous_modified_time:
                     # A module has been modified! Unload all modules
                     # not in the whitelist:
-                    sys.stderr.write('%s modified: all modules will be reloaded next run.\n'%module_file)
-                    for name in sys.modules.copy():
-                        if name not in self.whitelist:
-                            # This unloads a module. This is slightly
-                            # more general than reload(module), but
-                            # has the same caveats regarding existing
-                            # references. This also means that any
-                            # exception in the import will occur later,
-                            # once the module is (re)imported, rather
-                            # than now where catching the exception
-                            # would have to be handled differently.
-                            del sys.modules[name]
-                            if name in self.modified_times:
-                                del self.modified_times[name]
-                                
+                    message = '%s modified: all modules will be reloaded next run.\n'%module_file
+                    sys.stderr.write(message)
+                    with self.unloader_lock:
+                        # Acquire the import lock so that we don't unload
+                        # modules whilst an import is in progess:
+                        imp.acquire_lock()
+                        try:
+                            for name in sys.modules.copy():
+                                if name not in self.whitelist:
+                                    # This unloads a module. This is slightly
+                                    # more general than reload(module), but
+                                    # has the same caveats regarding existing
+                                    # references. This also means that any
+                                    # exception in the import will occur later,
+                                    # once the module is (re)imported, rather
+                                    # than now where catching the exception
+                                    # would have to be handled differently.
+                                    del sys.modules[name]
+                                    if name in self.modified_times:
+                                        del self.modified_times[name]
+                        finally:
+                            # We're done mucking around with the cached
+                            # modules, normal imports in other threads
+                            # may resume:
+                            imp.release_lock()
+                        
     def check_garbage_collection(self):
         counts, thresholds = gc.get_count(), gc.get_threshold()
         for generation, (count, threshold) in enumerate(zip(counts, thresholds)): 
@@ -63,4 +79,5 @@ class ModuleWatcher(object):
                 
 if __name__ == '__main__':
     module_watcher = ModuleWatcher()
+    import foo
     time.sleep(10)
