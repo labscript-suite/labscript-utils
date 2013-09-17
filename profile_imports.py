7399efe9e7cb75ff66d60bb3e3c6566394cd3d89
Chris Billington <chrisjbillington@gmail.com>
2013-09-17 17:08:59 +1000

    Modified import profiler. You now call enable(threshold) and disable to turn it on and off.
    And it doesn't have indentation issues due to not decrementing its depth counter
    when exceptions were raised in imports (as often happens, although they are expected and caught)

--- profile_imports.py
+++ profile_imports.py
@@ -1,27 +1,40 @@
-def profile_imports(threshold = 0.1):
-    import time
-    _old_import = __import__
-    class depth:
-        depth = -1
-        
-    def profiling_import(name, *args,**kwargs):
-        start_time = time.time()
-        depth.depth += 1
+import time
+
+class _ProfilingImporter(object):
+    def __init__(self):
+        self.normal_import = __import__
+        self.depth = 0
+        self.threshold = 0
         try:
-            result = _old_import(name, *args, **kwargs)
-            time_taken = time.time() - start_time
-            if time_taken > threshold:
-                print ' '*depth.depth + '[%.2f] import %s'%(time_taken, name)
-            return result
+            self.builtins_dict = __builtins__.__dict__
+        except AttributeError:
+            self.builtins_dict = __builtins__
+
+    def profiling_import(self, name, *args,**kwargs):
+        self.depth += 1
+        start_time = time.time()
+        try:
+            result = self.normal_import(name, *args, **kwargs)
         finally:
-            depth.depth -= 1
-    try:
-        builtins_dict = __builtins__.__dict__
-    except AttributeError:
-        # We must be the __main__ module. __builtins__ is already a dict here.
-         builtins_dict = __builtins__
-          
-    builtins_dict['__import__'] = profiling_import
+            self.depth -= 1
+        time_taken = time.time() - start_time
+        if time_taken > self.threshold:
+            print ' '*self.depth + '[%.2f] import %s'%(time_taken, name)
+        return result
+
+
+    def enable(self, threshold=0.1):
+        self.threshold = threshold
+        self.builtins_dict['__import__'] = self.profiling_import
+
+    def disable(self):
+        self.builtins_dict['__import__'] = self.normal_import
+    
+
+_profiling_importer = _ProfilingImporter()                   
+enable = _profiling_importer.enable
+disable = _profiling_importer.disable
 
 if __name__ == '__main__':
-    profile_imports()
+    enable(threshold=0.05)
+    import IPython
