019fd51846da53885e18c1831d8e5d147c994e3d
Phil Starkey <philipstarkey@users.noreply.github.com>
2013-11-29 15:12:31 +1100

    Calibration updates.. Hopefully works now!

--- aom.py
+++ aom.py
@@ -1,12 +1,12 @@
 from UnitConversionBase import *
 from numpy import *
 
-class SineAom(UnitConversionBase):
+class SineAom(UnitConversion):
     """
     AOM calibration P(A) is very close to a sine for dipole trap AOM!
     """
     base_unit = "Arb"
-    derived_units = ["Power"]
+    derived_units = ["Power", "fraction"]
 
     def __init__(self, calibration_parameters=None):
         # These parameters are loaded from a globals.h5 type file automatically
@@ -26,12 +26,33 @@ class SineAom(UnitConversionBase):
         f = self.parameters["f"]
         phase = self.parameters["phase"]
         c = self.parameters["c"]
-        return (arccos((power - c) / A) - phase) / (2*pi*f)
-
+        phi = arccos((power - c) / A) - phase
+        if phi <0:
+            phi += 2*pi
+        elif phi > 2*pi:
+            phi -= 2*pi
+        return phi / (2*pi*f)
+    
     def Power_from_base(self, amp):
         A = self.parameters["A"]
         f = self.parameters["f"]
         phase = self.parameters["phase"]
         c = self.parameters["c"]
 
-        return A * cos(2*pi*f*amp + phase) + c
+        return A * cos(2*pi*f*amp + phase) + c        
+
+    def fraction_to_base(self, fraction):
+        Pmax = self.parameters["A"] + self.parameters["c"]
+        Pmin = max(self.parameters["c"] - self.parameters["A"], 0)
+        P = (Pmax - Pmin) * fraction + Pmin
+        Amp = self.Power_to_base(P)
+        print "AOM amplitude", Amp
+        return Amp
+    
+    def fraction_from_base(self, amp):
+        P = self.Power_from_base(amp)
+        Pmax = self.parameters["A"] + self.parameters["c"]
+        Pmin = max(self.parameters["c"] - self.parameters["A"], 0)
+        fraction = (P - Pmin) / (Pmax - Pmin)
+        return fraction
+
