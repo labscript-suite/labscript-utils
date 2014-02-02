f3a4fb9b035b7dbfeb464deb71f3f8de7ba5e4ac
Phil Starkey <philipstarkey@users.noreply.github.com>
2014-02-02 22:54:40 +0000

    Removed the accidentally applied BSD license (using automatic script) as this file was not written by the labscript_suite team. See, remaining license at top of file for details.

--- qtwidgets/fingertab.py
+++ qtwidgets/fingertab.py
@@ -1,16 +1,3 @@
-#####################################################################
-#                                                                   #
-# fingertab.py                                                      #
-#                                                                   #
-# Copyright 2013, Monash University                                 #
-#                                                                   #
-# This file is part of the labscript suite (see                     #
-# http://labscriptsuite.org) and is licensed under the Simplified   #
-# BSD License. See the license.txt file in the root of the project  #
-# for the full license.                                             #
-#                                                                   #
-#####################################################################
-
 # Code by LegoStormtroopr
 #
 # License:
