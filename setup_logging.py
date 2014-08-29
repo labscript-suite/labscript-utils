eddb104341780b46b25786cddae229d13063d1aa
Chris Billington <chrisjbillington@gmail.com>
2014-08-29 11:57:54 -0400

    1.1.0-dev: Fixed copyright header in setup_logging.py - had wrong program name

--- setup_logging.py
+++ setup_logging.py
@@ -4,7 +4,7 @@
 #                                                                   #
 # Copyright 2013, Monash University                                 #
 #                                                                   #
-# This file is part of the program BLACS, in the labscript suite    #
+# This file is part of labscript_utils, in the labscript suite      #
 # (see http://labscriptsuite.org), and is licensed under the        #
 # Simplified BSD License. See the license.txt file in the root of   #
 # the project for the full license.                                 #
