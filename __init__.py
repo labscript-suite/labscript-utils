3ad5f5bda7fb5b24bd16e55922f339c525332af0
Chris Billington <chrisjbillington@gmail.com>
2014-10-14 17:02:40 -0400

    Added check_version function to labscript_utils where it can be shared by programs using it. Imports of this function will hard code the minimum version they require for importing this function! (2.1.0)

--- __init__.py
+++ __init__.py
@@ -11,4 +11,24 @@
 #                                                                   #
 #####################################################################
 
-__version__ = '2.0.0'
+__version__ = '2.1.0'
+
+
+class VersionException(Exception):
+    pass
+
+    
+def check_version(module_name, at_least, less_than, version=None):
+
+    def get_version_tuple(version_string):
+        version_tuple = [int(v.replace('+', '-').split('-')[0]) for v in version_string.split('.')]
+        while len(version_tuple) < 3:
+            version_tuple += (0,)
+        return version_tuple
+
+    if version is None:
+        version = __import__(module_name).__version__
+    at_least_tuple, less_than_tuple, version_tuple = [get_version_tuple(v) for v in [at_least, less_than, version]]
+    if not at_least_tuple <= version_tuple < less_than_tuple:
+        raise VersionException(
+            '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
