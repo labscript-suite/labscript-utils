585337b4776dd4b1350b0188c73ec0b047587bff
Jan Werkmann <jan.wrk.fb@gmail.com>
2017-08-01 21:18:42 +0200

    Qt5: Added version check for qtutils greater or equal 2.0.0

--- settings.py
+++ settings.py
@@ -10,6 +10,12 @@
 # for the full license.                                             #
 #                                                                   #
 #####################################################################
+try:
+    from labscript_utils import check_version
+except ImportError:
+    raise ImportError('Require labscript_utils > 2.1.0')
+
+check_version('qtutils', '2.0.0', '3.0.0')
 
 from qtutils.qt.QtCore import *
 from qtutils.qt.QtGui import *
