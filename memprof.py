867e16205da1263b5f8c8b14189c699f987c1050
Jan Werkmann <jan.wrk.fb@gmail.com>
2017-08-20 11:36:12 +0200

    Python3: keys to list in memprof.py

--- memprof.py
+++ memprof.py
@@ -33,7 +33,7 @@ def count_types():
     
 def write_to_file(types):
     with open('memprof.txt','w') as f:
-        names = types.keys()
+        names = list(types.keys())
         names.sort(key=lambda name: -types[name])
         for name in names:
             f.write(str(name).rjust(60) + ' ' + str(types[name]).rjust(8) + '\n')
