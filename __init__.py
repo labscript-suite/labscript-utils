45866ba3cc880916257acb36d9e6d9ee1a1286e7
Chris Billington <chrisjbillington@gmail.com>
2018-03-15 12:11:29 +1100

    memprof-filepath: Give memprof.start() a keyword argument to allow specifying a filename. Do not activate memprof by default - it is the importer's responsibility to call start(). Refactor into a class rather than using global variables.

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-__version__ = '2.6.1'
+__version__ = '2.6.2'
 
 
 import sys
