e5710075161252229e9cfb3b18f2f8f9bd601dad
Chris Billington <chrisjbillington@gmail.com>
2018-03-29 10:21:32 +1100

    logging-fix: For terminal logging, send warning and higher to stderr, info and lower to stdout.

--- setup_logging.py
+++ setup_logging.py
@@ -17,6 +17,14 @@ import logging, logging.handlers
 import __main__
 
 
+class LessThanFilter(logging.Filter):
+    def __init__(self, less_than):
+        self.less_than = less_than
+        logging.Filter.__init__(self)
+    def filter(self, record):
+        return record.levelno < self.less_than
+
+
 def setup_logging(program_name, log_level=logging.DEBUG, terminal_level=logging.INFO, maxBytes=1024*1024*50, backupCount=1):
     logger = logging.getLogger(program_name)
     # Clear any previously added handlers from the logger:
@@ -39,10 +47,17 @@ def setup_logging(program_name, log_level=logging.DEBUG, terminal_level=logging.
     handler.setLevel(log_level)
     logger.addHandler(handler)
     if sys.stdout is not None and sys.stdout.fileno() >= 0:
-        terminalhandler = logging.StreamHandler(sys.stdout)
-        terminalhandler.setFormatter(formatter)
-        terminalhandler.setLevel(terminal_level)
-        logger.addHandler(terminalhandler)
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(formatter)
+        stdout_handler.setLevel(terminal_level)
+        logger.addHandler(stdout_handler)
+        if sys.stderr is not None and sys.stderr.fileno() >= 0:
+            # Send warnings and greater to stderr instead of stdout:
+            stdout_handler.addFilter(LessThanFilter(logging.WARNING))
+            sterr_handler = logging.StreamHandler(sys.stderr)
+            sterr_handler.setFormatter(formatter)
+            sterr_handler.setLevel(logging.WARNING)
+            logger.addHandler(sterr_handler)
     else:
         # Prevent bug on windows where writing to stdout without a command
         # window causes a crash:
