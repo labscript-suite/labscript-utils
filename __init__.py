932d4e720006b384b97c3289bc43c6da3f85ca98
Chris Billington <chrisjbillington@gmail.com>
2018-09-28 15:13:22 -0400

    text_dedent: Add dedent() function, which dedents and removes newlines from strings so that you can more easily write multiline error message etc, such as: ``` def foo()     msg = """There was an error, which we have written in code         over multiple lines, but with indentation. This indentation         and these line breaks should be ignored.
                1. But this extra indentation
                2. and line endings
                3. should be included verbatim
    
            and then this text at the original indentation level should be
            unwrapped again.
            """
        raise ValueError(dedent(msg))
    ```
    
    This function is not quite the same as Python's textwrap.dedent, as this one
    excludes the first line from the indentation calculation, removes newlines,
    and includes lines with additional indentation verbatim. I've been using it to
    make long error messages without having to manually join strings with '+' for
    each line, which plays well with auto-wrapping text in text editors, e.g.
    alt-Q in sublime text.

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-__version__ = '2.7.2'
+__version__ = '2.8.0'
 
 
 import sys
@@ -92,3 +92,41 @@ def check_version(module_name, at_least, less_than, version=None):
     if version is None:
         raise ValueError('Invalid version string from package {}: {}'.format(module_name, version))
 
+
+def dedent(s):
+    """Remove leading spaces from the first line of a string, all common leading
+    indentation (spaces only) from subsequent lines, strip trailing spaces from all
+    lines and replace single newlines prior to lines with the common indentation with
+    spaces. Lines with additional indentation are kept verbatim. Good for unwrapping
+    error messages etc that are in code as multiline triple-quoted strings."""
+    # Strip trailing whitespace:
+    lines = [line.rstrip(' ') for line in s.splitlines()]
+    # Get common indentation from lines other than the first one:
+    indentation = float('inf')
+    for line in lines[1:]:
+        if line:
+            indentation = min(indentation, len(line) - len(line.lstrip(' ')))
+    if not lines[1:]:
+        indentation = 0
+    # Dedent the lines:
+    dedented_lines = []
+    for i, line in enumerate(lines):
+        if i == 0:
+            dedented_line = line.lstrip(' ')
+        else:
+            dedented_line = line[indentation:]
+        dedented_lines.append(dedented_line)
+    # Then add newline characters where we are going to keep them:
+    unwrapped_lines = []
+    for i, line in enumerate(dedented_lines):
+        if i == 0:
+            unwrapped_lines.append(line)
+        else:
+            previous_line = dedented_lines[i - 1]
+            # If either this line or the previous line is blank or starts with custom
+            # indentation, put this line on a newline rather than unwrapping it:
+            if any(not l or l.startswith(' ') for l in [line, previous_line]):
+                unwrapped_lines.append('\n' + line)
+            else:
+                unwrapped_lines.append(line)
+    return ' '.join(unwrapped_lines)
