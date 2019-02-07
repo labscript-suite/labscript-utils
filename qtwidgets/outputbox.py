0c862fabf66a850fa9266efda7d53952ea3a1148
Chris Billington <chrisjbillington@gmail.com>
2019-02-06 23:21:56 -0500

    zprocess_changes: Corrected misleading comment

--- qtwidgets/outputbox.py
+++ qtwidgets/outputbox.py
@@ -12,6 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 from labscript_utils import PY2
+
 if PY2:
     str = unicode
 
@@ -23,22 +24,22 @@ import qtutils.outputbox
 
 from labscript_utils.ls_zprocess import get_config, Context
 
+
 class OutputBox(qtutils.outputbox.OutputBox):
-    """A subclass of qtutils.OutputBox configured with security from labconfig.
-    A bit of magic happens at instantiation time to make it actually a subclass of
-    OutputBox, because we don't want this module to depend """
+    """A subclass of qtutils.outputbox.OutputBox configured with security from
+    labconfig."""
 
     def __init__(self, container, scrollback_lines=1000):
         config = get_config()
         if config['listen_localhost_only']:
-            bind_address='tcp://127.0.0.1'
+            bind_address = 'tcp://127.0.0.1'
         else:
-            bind_address='tcp://0.0.0.0'
+            bind_address = 'tcp://0.0.0.0'
         context = Context.instance()
         qtutils.outputbox.OutputBox.__init__(
             self,
             container=container,
             scrollback_lines=scrollback_lines,
             zmq_context=context,
-            bind_address=bind_address, 
+            bind_address=bind_address,
         )
