527c7518d770c9c3126e36d49f84eac692408f50
Chris Billington <chrisjbillington@gmail.com>
2019-03-15 14:18:56 -0400

    zlock_backcompat: Move ensure_connected_to_zlog and connect_to_zlock into ls_zprocess. Have Lock() ignore the read_only argument if the zlock server does not support it. This way callers may use the argument without having to check first. Have h5_lock pass the read_only keyword argument along, it will be ignored later if it is not supported.
    This was motivated by the runmanager sequence_id changes, which use a zlock Lock
    to serialise access to the file that stores the current sequence index. This
    way the runmanager code will work with old zlock servers.
    
    Patch version bump so the runmanager changes can rely on this.

--- h5_lock.py
+++ h5_lock.py
@@ -13,15 +13,10 @@
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 import sys
-from socket import gethostbyname
-from distutils.version import LooseVersion
 
-import zmq
-from labscript_utils.ls_zprocess import Lock, ProcessTree 
+from labscript_utils.ls_zprocess import Lock, connect_to_zlock_server
 
 from labscript_utils import check_version
-
-from zprocess import start_daemon
 
 from labscript_utils.shared_drive import path_to_agnostic
 from labscript_utils import PY2
@@ -36,14 +31,11 @@ import h5py
 # Depend on the fix since we no longer have the monkeypatch.
 check_version('h5py', '2.9', '3')
 
-DEFAULT_TIMEOUT = 45
-_server_supports_readwrite = False
-
 def hack_locks_onto_h5py():
     def __init__(self, name, mode=None, driver=None, libver=None, **kwds):
         if not isinstance(name, h5py._objects.ObjectID):
             kwargs = {}
-            if _server_supports_readwrite and mode == 'r':
+            if mode == 'r':
                 kwargs['read_only'] = True
             self.zlock = Lock(path_to_agnostic(name), **kwargs)
             self.zlock.acquire()
@@ -68,39 +60,6 @@ def hack_locks_onto_h5py():
     # new shiny locking ones:
     h5py.File.__init__ = __init__
     h5py.File.close = close 
-
-def connect_to_zlock_server():
-    # Ensure we are connected to a zlock server, and start one if one is supposed
-    # to be running on localhost but is not.
-    client = ProcessTree.instance().zlock_client
-    if gethostbyname(client.host) == gethostbyname('localhost'):
-        try:
-            # short connection timeout if localhost, don't want to
-            # waste time:
-            client.ping(timeout=0.05)
-        except zmq.ZMQError:
-            # No zlock server running on localhost. Start one. It will run forever, even
-            # after this program exits. This is important for other programs which might
-            # be using it. I don't really consider this bad practice since the server is
-            # typically supposed to be running all the time:
-            start_daemon(
-                [sys.executable, '-m', 'labscript_utils.zlock', '--daemon']
-            )
-            # Try again. Longer timeout this time, give it time to start up:
-            client.ping(timeout=15)
-    else:
-        client.ping()
-
-    # Check if the zlock server supports read-write locks:
-    global _server_supports_readwrite
-    if hasattr(client, 'get_protocol_version'):
-        version = client.get_protocol_version()
-        if LooseVersion(version) >= LooseVersion('1.1.0'):
-            _server_supports_readwrite = True
-
-    # The user can call these functions to change the timeouts later if they
-    # are not to their liking:
-    client.set_default_timeout(DEFAULT_TIMEOUT)
 
 
 connect_to_zlock_server()
