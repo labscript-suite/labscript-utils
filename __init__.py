bada45e2c55e813a73bdc8cd210d5f65fa11e373
Chris Billington <chrisjbillington@gmail.com>
2019-03-29 13:37:19 -0400

    JSON_bytestrings: Version bump so bytestring serialisation can be relied upon

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-__version__ = '2.12.0'
+__version__ = '2.12.1'
 
 
 import sys
