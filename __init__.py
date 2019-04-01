0d1bdb0d22a85b5922685fb61d827a3268d3ea6f
Chris Billington <chrisjbillington@gmail.com>
2019-04-01 11:50:56 -0400

    Patch version bump for bytestring serialisation

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-__version__ = '2.12.1'
+__version__ = '2.12.2'
 
 
 import sys
