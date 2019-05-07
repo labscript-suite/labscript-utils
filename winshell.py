4fa31cf39efd87d82201fcc10fdaca120b2e9c69
Chris Billington <chrisjbillington@gmail.com>
2019-05-06 21:06:47 -0400

    winlauncher: Move functionality from the installer into winshell module for consistent app descriptions (app descriptions in the taskbar vs start menu differed previously), and to allow apps to know at run time what the command to launch them is, since they need to set it as their relaunch command.
    Add launcher that runs apps in a conda environment, and that if run with pythonw
    runs apps in python.exe without a console window. This solves a number of issues
    relating to pythonw and makes zprocess's output redirection more comprehensive.
    For example, C/cython extensions or non-python subprocess output cannot be
    redirected to output boxes when running under pythonw. Starting the launcher
    with pythonw.exe and then having it run the target with python.exe allows for
    no console window to be visible, even for a moment.
    
    Using this launcher instead of cwp.py from the menuinst project is also required
    because our apps need more environment variables pertaining to conda than are
    set by cwp.py. Further, cwp.py unconditionally sets the working directory
    to the user's documents directory, whereas we have configured out own working
    directories that we would prefer our apps to run in.

--- winshell.py
+++ winshell.py
@@ -13,6 +13,52 @@ for path in sys.path:
 else:
     labscript_installation = '<not_installed>'
 
+APPS = ['runmanager', 'runviewer', 'blacs', 'lyse']
+
+def launcher_name(appname):
+    """Return the name of the launcher file for a given program. 
+    This will be used for the launchers and start menu shortcuts"""
+    name = 'labscript suite'
+    # Add conda env name, if any and if not the base env:
+    env = os.getenv('CONDA_DEFAULT_ENV')
+    if env is not None and env != 'base':
+        name += ' (%s)' % env
+    name += ' - %s' % appname
+    if os.name == 'nt':
+        name += '.lnk'
+    return name
+
+def launch_command(appname):
+    """Return target, arguments for launching the given app. Includes hoop-jumping for
+    conda environments"""
+    target = sys.executable.lower()
+    if not target.endswith('w.exe'):
+        target = target.lower().replace('.exe', 'w.exe')
+
+    # Wrap the command in call to our launcher script:
+
+    WINLAUNCHER = os.path.join(
+            labscript_installation, 'labscript_utils', 'winlauncher.py'
+        )
+
+    args = [WINLAUNCHER]
+
+    if os.getenv('CONDA_DEFAULT_ENV') is not None:
+        # Tell the launcher script to configure the given conda environment:
+        args += ['-n', os.getenv('CONDA_DEFAULT_ENV'), '-p', os.getenv('CONDA_PREFIX')]
+
+
+    # Add the actual path to the __main__ script of the app:
+    args += [os.path.join(labscript_installation, appname, '__main__.py')]
+
+    # Quote for spaces etc in the target and args list:
+    target = '"%s"' % target
+    arglist = ' '.join(['"%s"' % arg for arg in args])
+
+    return target, arglist
+
+
+
 # Including the install directory and python interpreter in the below AppId strings
 # ensures they are unique to the install location and any conda env or virtualenv. If
 # they were not, then installing to one directory, uninstalling, and reinstalling to
@@ -20,15 +66,12 @@ else:
 # and icons are broken. This if of particular importance when developing on the same
 # machine as you are deploying to.
 _INSTALL = '%s.%s' % (labscript_installation, sys.executable)
-appids = {'runmanager': 'Monashbec.Labscript.Runmanager.%s'%_INSTALL,
-         'runviewer': 'Monashbec.Labscript.Runviewer.%s'%_INSTALL,
-         'blacs': 'Monashbec.Labscript.Blacs.%s'%_INSTALL,
-         'lyse': 'Monashbec.Labscript.Lyse.%s'%_INSTALL}
+appids = {
+    app: 'Monashbec.Labscript.%s.%s' % (app.capitalize(), _INSTALL) for app in APPS
+}
 
-app_descriptions = {'runmanager': 'runmanager - the labscript suite',
-                   'runviewer': 'runviewer - the labscript suite',
-                   'blacs': 'blacs - the labscript suite',
-                   'lyse': 'lyse - the labscript suite'}
+# The display name of the apps:
+app_descriptions = {app: launcher_name(app).replace('.lnk', '') for app in APPS}
 
 if os.name == 'nt':
     from win32com.shell import shellcon
@@ -44,35 +87,70 @@ def _check_windows():
         msg = "winshell functions are Windows only"
         raise RuntimeError(msg)
 
-def make_shortcut(path, target, arguments, working_directory, icon_path, description, appid):
+def make_shortcut(appname):
+    """Create a shortcut file in the labscript suite install dir for the given app"""
+    shortcut_path = os.path.join(labscript_installation, launcher_name(appname))
+    app_dir = os.path.join(labscript_installation, appname)
     _check_windows()
     shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortcut(path)
+    shortcut = shell.CreateShortcut(shortcut_path)
+    target, args = launch_command(appname)
     shortcut.TargetPath = target
-    shortcut.Arguments = arguments
-    shortcut.WorkingDirectory = working_directory
-    shortcut.IconLocation = icon_path
-    shortcut.Description = description
+    shortcut.Arguments = args
+    shortcut.WorkingDirectory = '"%s "' % app_dir
+    shortcut.IconLocation = os.path.join(app_dir, appname + '.ico')
+    shortcut.Description = app_descriptions[appname]
     shortcut.save()
 
     store = propsys.SHGetPropertyStoreFromParsingName(
-        path, None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore
+        shortcut_path, None, shellcon.GPS_READWRITE, propsys.IID_IPropertyStore
     )
     store.SetValue(
         pscon.PKEY_AppUserModel_ID,
-        propsys.PROPVARIANTType(str(appid), pythoncom.VT_LPWSTR),
+        propsys.PROPVARIANTType(str(appids[appname]), pythoncom.VT_LPWSTR),
     )
     store.Commit()
 
+    return shortcut_path
 
-def set_appusermodel(window_id, appid, icon_path, relaunch_command, relaunch_display_name):
+
+def set_appusermodel(
+    window_id,
+    appid=None,
+    icon_path=None,
+    relaunch_command=None,
+    relaunch_display_name=None,
+    appname=None,
+):
+    """Set the appID details for the window, configuring how it appears in the taskbar
+    and its pinning/relaunching behaviour. If appid, icon_path, relaunch_command or
+    relaunch_display_name are None, they will be inferred from the appname, which must
+    not be None if the other arguments are not provided."""
     _check_windows()
+    if appid is None:
+        appid = appids[appname]
+    if icon_path is None:
+        icon_path = os.path.join(labscript_installation, appname, appname + '.ico')
+    if relaunch_command is None:
+        target, args = launch_command(appname)
+        relaunch_command = ' '.join([target, args])
+    if relaunch_display_name is None:
+        relaunch_display_name = app_descriptions[appname]
+
     store = propsys.SHGetPropertyStoreForWindow(window_id, propsys.IID_IPropertyStore)
     id = store.GetValue(pscon.PKEY_AppUserModel_ID)
     store.SetValue(pscon.PKEY_AppUserModel_ID, propsys.PROPVARIANTType(appid))
-    store.SetValue(pscon.PKEY_AppUserModel_RelaunchCommand, propsys.PROPVARIANTType(relaunch_command))
-    store.SetValue(pscon.PKEY_AppUserModel_RelaunchDisplayNameResource, propsys.PROPVARIANTType(relaunch_display_name))
-    store.SetValue(pscon.PKEY_AppUserModel_RelaunchIconResource, propsys.PROPVARIANTType(icon_path))
+    store.SetValue(
+        pscon.PKEY_AppUserModel_RelaunchCommand,
+        propsys.PROPVARIANTType(relaunch_command),
+    )
+    store.SetValue(
+        pscon.PKEY_AppUserModel_RelaunchDisplayNameResource,
+        propsys.PROPVARIANTType(relaunch_display_name),
+    )
+    store.SetValue(
+        pscon.PKEY_AppUserModel_RelaunchIconResource, propsys.PROPVARIANTType(icon_path)
+    )
 
 def add_to_start_menu(shortcut):
     _check_windows()
