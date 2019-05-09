f63f86f511247c9003d284ee60fd07f53caba0d9
Chris Billington <chrisjbillington@gmail.com>
2019-05-09 19:02:13 -0400

    modulewatcher-whitelist: Acquire import lock when checking modules as well as unloading them. Add all modules we are not interested in to the whitelist so that we don't check things like whether they exist on disk again and again after the first time.
    Fix bug where module objects were added to the whitelist instead of their name.

--- modulewatcher.py
+++ modulewatcher.py
@@ -33,12 +33,13 @@ PKGDIRS = [
 PKGDIRS += site.getsitepackages()
 PKGDIRS = set(PKGDIRS)
 
+
 class ModuleWatcher(object):
     def __init__(self, debug=False):
         self.debug = debug
         # A lock to hold whenever you don't want modules unloaded:
         self.lock = threading.Lock()
-            
+
         # The whitelist is the list of names of currently loaded modules:
         self.whitelist = set(sys.modules)
         self.meta_whitelist = list(sys.meta_path)
@@ -46,28 +47,41 @@ class ModuleWatcher(object):
         self.main = threading.Thread(target=self.mainloop)
         self.main.daemon = True
         self.main.start()
-        
+
     def mainloop(self):
         while True:
             time.sleep(1)
             with self.lock:
-                if self.check():
-                    self.unload()
-            
+                # Acquire the import lock so that we don't unload modules whilst an
+                # import is in progess:
+                imp.acquire_lock()
+                try:
+                    if self.check():
+                        self.unload()
+                finally:
+                    # We're done mucking around with the cached modules, normal imports
+                    # in other threads may resume:
+                    imp.release_lock()
+
     def check(self):
         unload_required = False
         # Look through currently loaded modules:
-        for name, module in sys.modules.copy().items():
+        for name, module in sys.modules.items():
             # Look only at the modules not in the the whitelist:
-            if name not in self.whitelist and hasattr(module, '__file__'):
-                # Only consider modules which are .py files, no C extensions or builtin
-                # modules:
-                if module.__file__ is None:
+            if name not in self.whitelist:
+                # Only consider modules which have a non-None __file__ attribute, are
+                # .py (or .pyc) files (no C extensions or builtin modules), that exist
+                # on disk, and that aren't in standard package directories. Add modules
+                # we won't consider to the whitelist so that we don't consider them in
+                # future calls.
+                if getattr(module, '__file__', None) is None:
+                    self.whitelist.add(name)
                     continue
                 module_file = module.__file__
                 if module_file.endswith('.pyc'):
                     module_file = os.path.splitext(module_file)[0] + '.py'
                 if not module_file.endswith('.py') or not os.path.exists(module_file):
+                    self.whitelist.add(name)
                     continue
                 if any(module_file.startswith(s + os.path.sep) for s in PKGDIRS):
                     # Whitelist modules in package install directories:
@@ -96,35 +110,20 @@ class ModuleWatcher(object):
             for name in sorted(self.whitelist):
                 print("    " + name)
             print("\nModuleWatcher: modules unloaded:")
-        # Acquire the import lock so that we don't unload
-        # modules whilst an import is in progess:
-        imp.acquire_lock()
-        try:
             for name in sorted(sys.modules):
                 if name not in self.whitelist:
-                    # This unloads a module. This is slightly
-                    # more general than reload(module), but
-                    # has the same caveats regarding existing
-                    # references. This also means that any
-                    # exception in the import will occur later,
-                    # once the module is (re)imported, rather
-                    # than now where catching the exception
-                    # would have to be handled differently.
+                    # This unloads a module. This is slightly more general than
+                    # reload(module), but has the same caveats regarding existing
+                    # references. This also means that any exception in the import will
+                    # occur later, once the module is (re)imported, rather than now
+                    # where catching the exception would have to be handled differently.
                     del sys.modules[name]
                     if name in self.modified_times:
                         del self.modified_times[name]
                     if self.debug:
                         print("    " + name)
-            # Replace sys.meta_path with the cached whitelist,
-            # effectively removing all since-added entries from
-            # it. Replacement is done in-place in case other code
-            # holds references to sys.meta_path, and to preserve
-            # order, since order is relevant.
+            # Replace sys.meta_path with the cached whitelist, effectively removing all
+            # since-added entries from it. Replacement is done in-place in case other
+            # code holds references to sys.meta_path, and to preserve order, since order
+            # is relevant.
             sys.meta_path[:] = self.meta_whitelist
-        finally:
-            # We're done mucking around with the cached
-            # modules, normal imports in other threads
-            # may resume:
-            imp.release_lock()
-                            
-                            
