040063714e27a52c8c0d1b831ee1647f18e5fd55
Chris Billington <chrisjbillington@gmail.com>
2019-05-29 14:53:17 -0400

    Patch version bump for recent bugfixes

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@
 #####################################################################
 from __future__ import division, unicode_literals, print_function, absolute_import
 
-__version__ = '2.13.0'
+__version__ = '2.13.1'
 
 
 import sys
