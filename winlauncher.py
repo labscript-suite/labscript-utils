9fe988e876fd5123ee090e4c6bb092379394bcd6
chrisjbillington <chrisjbillington@gmail.com>
2020-05-02 19:24:55 -0400

    Allow winlauncher to be an entry_point for apps
    Make it so that another package may define entry points in its
    setup() as (using runmanager as an example):
    
    ```
    entry_points={
            'console_scripts': ['runmanager = winlauncher:main'],
            'gui_scripts': ['runmanagerw = winlauncher:main'],
        },
    ```
    
    and that this will have winlauncher run the `__main__.py` of the module
    in question, after activating the conda environment associated with the
    Python executable that was used to install the entry point.
    
    The convention is that the entry points should be named `<modulename>`
    and `<modulenamew>` (for the non-console version). `winlauncher` sees
    this as its sys.argv[0] and therefore knows which module to run.
    
    Conda environment setup is now inspected based on the location of the
    Python executable, so is no longer passed in on the command line.
    
    This setup makes there be a runmanager.exe and runmanagerw.exe within
    the Scripts directory of the Python installation, that can be used to
    start runmanager directly so long as one has the full path to it.
    
    Yet to do is update winshell to set relaunch commands correctly in light
    of this change.
    
    Conda env changes are untested for now.

--- winlauncher.py
+++ winlauncher.py
@@ -1,83 +1,130 @@
 import sys
 import os
-from os.path import join, pathsep, dirname
-import subprocess
-import argparse
+import importlib.util
 
-parser = argparse.ArgumentParser(
-    description="""A launcher for running Python scripts/apps on Windows, potentially in
-        conda environments. This script can be run with either pythonw.exe or
-        python.exe, but in the former case it will launch the app using python.exe with
-        a hidden console window. This prevents a number of issues with using
-        pythonw.exe, but without having to show a console window. If using a conda
-        environment, then any python interpreter may be used to invoke this script, and
-        the 'python' command from within the environment will be used to run the target
-        python script. If not using a conda environment, the python interpreter used to
-        invoke this script (or the corresponding python.exe if pythonw.exe was used)
-        will be used to invoke the target script."""
-)
+def find_conda_env():
+    """inspect whether sys.executable is within a conda environment and if it is, return
+    the environment name and prefix. Otherwise return None, None"""
+    prefix = os.path.dirname(sys.executable)
+    if not os.path.isdir(os.path.join(prefix, 'conda-meta')):
+        # Not a conda env
+        return None, None
+    if os.path.isdir(os.path.join(prefix, 'condabin')):
+        # It's the base conda env:
+        return 'base', prefix
+    # Not the base env: its name is the directory basename:
+    return os.path.basename(prefix), prefix
 
-parser.add_argument(
-    '-n',
-    '--name',
-    type=str,
-    help="""Name of the conda environment to be used, if any.""",
-)
 
-parser.add_argument(
-    '-p',
-    '--prefix',
-    type=str,
-    help="""Prefix of the conda environment, if any.""",
-)
-
-parser.add_argument(
-    'script',
-    type=str,
-    help="""Python script to run. Note: if you wish to do something other than run a
-        script by its filepath, you may pass '--' in place of this argument, then all
-        subsequent arguments will be passed to the Python interpreter verbatim. This can
-        be used for example to run a module as __main__ using the '-m' flag, i.e.
-        winshell.py <other arguments> -- -m <module>""",
-)
-
-parser.add_argument(
-    'args',
-    metavar='args',
-    type=str,
-    nargs=argparse.REMAINDER,
-    help='Arguments to pass to target Python script or interpreter',
-)
-
-CREATE_NO_WINDOW = 1 << 27
-args = parser.parse_args()
-prefix = args.prefix
-popen_kwargs = {}
-
-if prefix is not None:
-    # Environment manipulation copied from cwp.py from the menuinst project. We're not
-    # using that script because we do not want to do an os.chdir into the documents
-    # folder as it does, and we also need to set the CONDA_DEFAULT_ENV environment
-    # variable since our programs need to know the name of the environment they're in,
-    # and not just the prefix. Using our own launcher also allows us to swap out
-    # pythonw.exe for python.exe without creating a visible console, which otherwise
-    # requires yet another layer of subprocesses to achieve.
-    new_paths = pathsep.join([prefix,
-                         join(prefix, "Library", "mingw-w64", "bin"),
-                         join(prefix, "Library", "usr", "bin"),
-                         join(prefix, "Library", "bin"),
-                         join(prefix, "Scripts")])
+def activate_conda_env(name, prefix):
+    """Modify environment variables so as to effectively activate the given conda env
+    from the perspective of child processes. If the conda env appears to already be
+    active, do nothing. Does not set environment variables, instead returns a copy that
+    may be passed to subprocess.Popen as the env arg."""
     env = os.environ.copy()
-    env['PATH'] = new_paths + pathsep + env['PATH']
+    if env['CONDA_DEFAULT_ENV'] == name and env['CONDA_PREFIX'] == prefix:
+        # Env is already active
+        return
+    env['CONDA_DEFAULT_ENV'] = name
     env['CONDA_PREFIX'] = prefix
-    env['CONDA_DEFAULT_ENV'] = args.name
-    python = 'python'
-    popen_kwargs['env'] = env
-    popen_kwargs['creationflags'] = CREATE_NO_WINDOW
-elif 'pythonw.exe' in sys.executable.lower():
-    python = join(dirname(sys.executable), 'python.exe')
-    popen_kwargs['creationflags'] = CREATE_NO_WINDOW
-else:
-    python = sys.executable
+    new_paths = os.path.pathsep.join(
+        [
+            prefix,
+            os.path.join(prefix, "Library", "mingw-w64", "bin"),
+            os.path.join(prefix, "Library", "usr", "bin"),
+            os.path.join(prefix, "Library", "bin"),
+            os.path.join(prefix, "Scripts"),
+        ]
+    )
+    existing_paths = env.get('PATH', '')
+    # Avoid a leading path separator in the PATH variable:
+    if existing_paths:
+        env['PATH'] = new_paths + os.path.pathsep + existing_paths
+    else:
+        env['PATH'] = new_paths
 
-sys.exit(subprocess.call([python, args.script] + args.args, **popen_kwargs))
+    return env
+
+def run(*args):
+    """Runs a child Python subprocess, passing it the given argument list. If
+    sys.executable is pythonw.exe, then the child process will be run with the
+    corresponding python.exe with a hidden console window. Otherwise it will be run with
+    sys.executable. If sys.executable is within a conda environment, then the child
+    process's environment will be modified to have the effect of activating the
+    environment."""
+    import subprocess
+
+    CREATE_NO_WINDOW = 1 << 27 # TODO: can use subprocess.CREATE_NO_WINDOW in py3.7+
+
+    popen_kwargs = {}
+
+    envname, prefix = find_conda_env()
+    if envname is not None:
+        env = activate_conda_env(envname, prefix)
+        popen_kwargs['env'] = env
+
+    python = sys.executable
+    if os.path.basename(python).lower() == 'pythonw.exe':
+        python = os.path.join(os.path.dirname(python), 'python.exe')
+        popen_kwargs['creationflags'] = CREATE_NO_WINDOW
+
+    return subprocess.call([python] + list(args), **popen_kwargs)
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser( description="""A launcher for running Python
+        scripts/apps on Windows, potentially in conda environments. Run a child Python
+        subprocess, passing it the given argument list. If the Python interpreter used
+        to invoke this script is Python.exe, then it will be used to invoke the
+        subprocess, but if it is Pythonw.exe, the child will instead be run with the
+        corresponding Python.exe with a hidden console window. This prevents a number of
+        issues with using Pythonw.exe, but without having to show a console window. If
+        the Python interpreter is within a conda environment, then the child process's
+        environment will be modified to have the effect of activating the environment.
+        If this script is invoked as an entry_point of another package, it will inspect
+        sys.argv[0] to find the name of the entry_point script. The basename of the
+        script, (excluding a '.exe' suffix (or 'w.exe' if a gui_script) will be
+        interpreted as a module name, and that module - or its __main__.py if it's a
+        package - will be run. Note that a package's __init__.py will not be run first
+        as is the case with `python -m package_name`. This is a performance optimisation
+        to allow the program to say, display a splash screen as soon as possible during
+        startup. If it is necessary for __init__.py to run, the application's
+        __main__.py should import it. In this way, an application may define gui_scripts
+        and console_scripts entry_points named <modulename> and <modulenamew> that point
+        to winlauncher:main to create launcher scripts."""
+    )
+
+    parser.add_argument(
+        'args',
+        metavar='args',
+        type=str,
+        nargs=argparse.REMAINDER,
+        help="""Arguments to pass to the child Python interpreter. In the simplest case
+            this simply the path to a script to be run, but may be '-m module_name' or
+            any other arguments accepted by the Python' command""",
+    )
+    args = parser.parse_args().args
+    if os.path.abspath(__file__) != os.path.abspath(sys.argv[0]):
+        # we're being run as an entry_point for an application. Insert that
+        # application's main script at the start of the argument list.
+        module_name = os.path.basename(sys.argv[0]).lower()
+        if os.path.basename(sys.executable).lower() == 'pythonw.exe':
+            module_name = module_name.rsplit('w', 1)[0]
+        # Find the import path of the module:
+        spec = importlib.util.find_spec(module_name)
+        if spec is None or spec.origin is None:
+            raise ModuleNotFoundError(module_name)
+        if spec.parent:
+            # A package:
+            script = os.path.join(os.path.dirname(spec.origin), '__main__.py')
+        else:
+            # A single-file module:
+            script = spec.origin
+        # Insert at the start of the argument list:
+        args = [script] + args
+
+    sys.exit(run(*args))
+
+if __name__ == '__main__':
+    main()
