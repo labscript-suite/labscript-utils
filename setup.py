1a28a7fe60e4a5f594a30e8e36435a26c1b62a0c
chrisjbillington <chrisjbillington@gmail.com>
2020-05-21 19:27:32 -0400

    Remove code specific to building with setuptools_conda
    In the latest setuptools_conda, this is not necessary - the dist_conda
    command is available without having to explicitly declare it in
    setup.py, and the file path issue was sorted out by having
    setuptools_conda build wheels as an intermediate, more closely matching
    what a pip install does (this is the most commonly used way to build
    conda packages anyway, so it's what setuptools_conda should have been
    doing all along)
    
    Also, setuptools_conda introspects python versions from package
    classifiers, so that doesn't need to be declared.

--- setup.py
+++ setup.py
@@ -1,32 +1,9 @@
 import os
 from setuptools import setup
-from distutils import sysconfig
-
-try:
-    from setuptools_conda import dist_conda
-    CMDCLASS = {"dist_conda": dist_conda}
-except ImportError:
-    CMDCLASS = {}
-
-if "CONDA_BUILD" in os.environ:
-    # Various packaging schemes are variously unhappy with how to include the .pth file
-    # in site-packages. Conda is happy if we specify it with data_files and an absolute
-    # path, whereas basically everything else (pip, setup.py install, bdist,
-    # bdist_wheel) is happy if we specify it as package_data one level up.
-    DATA_FILES = [(sysconfig.get_python_lib(), ["labscript-suite.pth"])]
-    PACKAGE_DATA = {}
-else:
-    DATA_FILES = []
-    PACKAGE_DATA = {"labscript_suite": [os.path.join("..", "labscript-suite.pth")]}
 
 VERSION_SCHEME = {
     "version_scheme": os.getenv("SCM_VERSION_SCHEME", "guess-next-dev"),
     "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
 }
 
-setup(
-    use_scm_version=VERSION_SCHEME,
-    cmdclass=CMDCLASS,
-    data_files=DATA_FILES,
-    package_data=PACKAGE_DATA,
-)
+setup(use_scm_version=VERSION_SCHEME)
