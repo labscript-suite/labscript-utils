ed769017d81b26bb4470dbc56e1a101037e59b25
chrisjbillington <chrisjbillington@gmail.com>
2020-06-24 01:23:44 -0400

    Move copying of .pth file into setup.py
    Use a custom develop command to copy the .pth file into site-packages
    when `python setup.py develop` is run - this is what pip does under the
    hood for `pip install -e`.
    
    No longer need desktop-app to locate site-packages, as the setuptools
    command class knows it as `self.install_dir`.
    
    The file is not removed by `pip uninstall`, but due to the `try:
    except:`s in the `.pth` file, leaving it behind is harmless.
    
    It is removed if the user runs whatever `setup.py` command undoies

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import os
 from setuptools import setup
+from setuptools.command.develop import develop
+from distutils import log
+
+
+class develop_command(develop):
+    """Custom develop command which installs the .pth file to site-packages for editable
+    installs."""
+    def run(self):
+        path = os.path.join(self.install_dir, 'labscript-suite.pth')
+        super().run()
+        if not self.uninstall:
+            log.info(f'Copying labscript-suite.pth to {path}')
+            if not self.dry_run:
+                self.copy_file('labscript-suite.pth', path)
+
 
 VERSION_SCHEME = {
     "version_scheme": os.getenv("SCM_VERSION_SCHEME", "guess-next-dev"),
     "local_scheme": os.getenv("SCM_LOCAL_SCHEME", "node-and-date"),
 }
 
-setup(use_scm_version=VERSION_SCHEME)
+setup(use_scm_version=VERSION_SCHEME, cmdclass={'develop': develop_command})
