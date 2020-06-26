ee33287dc8f6137b5782385376e0e6bc2405dd90
philipstarkey <philipstarkey@users.noreply.github.com>
2020-06-26 19:05:45 +1000

    Relocate labscript_devices import machinery
    The labscript_devices import machinery has now been relocated to labscript_utils as the first step in breaking the circular dependency between labscript-devices and blacs packages.
    
    The code in _import_machinery.py is equivalent to that in labscript_devices/__init__.py exept that `__name__` has been replaced with `'labscript_devices'`.
    
    There is also some code that chooses whether to use the old labscript_devices code or the new labscript_utils code depending on the version of labscript_devices. The specific version will need to be adjusted to match whatever version removes the code from labscript_devices (and even then, would not catch the case of users with local changes in a development install) so this may not be the best approach, but I couldn't think of another way to ensure compatibility between old and new labscript-suite components without reintroducing a different circular dependency.
    
    The labscript_devices `__init__.py` file can then be replaced with `from labscript_utils.labscript_devices import *` so as not to need to update every device class file.

---
+++ labscript_utils/labscript_devices/__init__.py
@@ -0,0 +1,13 @@
+import importlib
+import packaging.version
+
+labscript_device_version_str = importlib.metadata.version('labscript_devices')
+labscript_devices_version = packaging.version.parse(labscript_device_version_str)
+
+# import and use the labscript_devices code if an old version of the package is used.
+# This ensures that new labscript_utils with old labscript_devices doesn't attempt to
+# register labscript_devices classes twice
+if labscript_devices_version < packaging.version.parse('3.1.0.dev16'):
+    from labscript_devices import *
+else:
+    from ._import_machinery import *
