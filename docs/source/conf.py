83c6ded5e7c6911aaf096ef3121966e013ca9a39
Phil Starkey <philipstarkey@users.noreply.github.com>
2020-11-02 13:04:42 +1100

    Attempt to fix the site module issue on readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
 ]
 
 autodoc_typehints = 'description'
+autodoc_mock_imports = ["site"]
 
 # Prefix each autosectionlabel with the name of the document it is in and a colon
 autosectionlabel_prefix_document = True
